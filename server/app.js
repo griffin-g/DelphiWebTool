39a5fa78f4e5a1f12088c4e7717c5b87c651ac16
WilliamHeinecke <116041964+WilliamHeinecke@users.noreply.github.com>
2024-10-31 11:32:36 -0700

    surveys crud functions

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/userRouter");
+var surveysRouter = require("./routes/surveysRouter");
 const cors = require("cors");
 var app = express();
 const db = require("./models");
@@ -21,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
+app.use("/surveys", surveysRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
