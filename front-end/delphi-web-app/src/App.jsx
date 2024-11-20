a08735ccea2c6a3e8b5a8cd1ada353946b542210
WilliamHeinecke <116041964+WilliamHeinecke@users.noreply.github.com>
2024-11-19 16:34:42 -0800

    created route and page

--- front-end/delphi-web-app/src/App.jsx
+++ front-end/delphi-web-app/src/App.jsx
@@ -3,13 +3,14 @@ import "survey-core/defaultV2.min.css";
 import "./survey-creator.css";
 import { Routes, Route } from "react-router-dom";
 import CreateSurvey from "./pages/CreateSurvey";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div>
-      <h1>some text</h1>
       <Routes>
         <Route path="/create-survey" element={<CreateSurvey></CreateSurvey>} />
+        <Route path="/about-us" element={<AboutUs />} />
       </Routes>
     </div>
   );
